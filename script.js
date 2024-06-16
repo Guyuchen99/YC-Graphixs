fdcddc76197daab762683793638f5e6e911d04ae
Guyuchen99 <93961613+Guyuchen99@users.noreply.github.com>
2024-06-16 13:17:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -31,57 +31,61 @@ $(document).ready(function () {
   ScrollReveal().reveal(".home-content h1", { origin: "left" });
 
   // Typed JS
-  var typed = new Typed(".typing1", {
-    strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
-    startDelay: 1500,
-    typeSpeed: 100,
-    backSpeed: 75,
-    backDelay: 1500,
-    loop: true,
-  });
+  // var typed = new Typed(".typing1", {
+  //   strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
+  //   startDelay: 1500,
+  //   typeSpeed: 100,
+  //   backSpeed: 75,
+  //   backDelay: 1500,
+  //   loop: true,
+  // });
 
-  var typed = new Typed(".typing2", {
-    strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
-    typeSpeed: 100,
-    backSpeed: 75,
-    backDelay: 1500,
-    loop: true,
-  });
+  // var typed = new Typed(".typing2", {
+  //   strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
+  //   typeSpeed: 100,
+  //   backSpeed: 75,
+  //   backDelay: 1500,
+  //   loop: true,
+  // });
 
   // Shop Item Display
   let shopItems = document.querySelector(".shop .shop-items");
   shopItems.innerHTML += shopItems.innerHTML;
-  
+
   let shopBtn = document.querySelectorAll(".shop .shop-btn-container .shop-btn");
   let speed = -2;
 
   let move = () => {
-    // Calculate the new position
     let currentLeft = parseInt(shopItems.style.left) || 0;
     let newLeft = currentLeft + speed;
 
-    // Reset position if we have scrolled past half the width of the container
     if (newLeft <= -shopItems.offsetWidth / 2) {
-      newLeft = 0; 
+      newLeft = 0;
     }
-    // Reset position if we have scrolled too far to the right (unlikely with negative speed)
+
     if (newLeft > 0) {
       newLeft -= shopItems.offsetWidth / 2;
     }
 
-    // Update the position
     shopItems.style.left = newLeft + "px";
   };
 
   let timer = setInterval(move, 30);
 
-  shopItems.addEventListener("mouseover", function () {
+  const stopScroll = () => {
     clearInterval(timer);
-  });
+  };
 
-  shopItems.addEventListener("mouseout", function () {
+  const resumeScroll = () => {
     timer = setInterval(move, 30);
-  });
+  };
+
+  shopItems.addEventListener("mouseover", stopScroll);
+  shopItems.addEventListener("mouseout", resumeScroll);
+
+  shopItems.addEventListener("touchstart", stopScroll);
+  shopItems.addEventListener("touchend", resumeScroll);
+  shopItems.addEventListener("touchcancel", resumeScroll);
 
   shopBtn[0].onclick = function () {
     speed = -2;
