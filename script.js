5e01ed3da2c53ec7a1adf5eaad0f730864ae9ee3
Guyuchen99 <93961613+Guyuchen99@users.noreply.github.com>
2024-06-16 12:38:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ $(document).ready(function () {
   $(window).scroll(function () {
     // "this" refers to window
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $(".header").addClass("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      $(".header").removeClass("sticky");
     }
+  });
+
+  //Toggle Navbar
+  $(".navbar-icon").click(function () {
+    $(".header .navbar-menu").toggleClass("active");
+    $(".header .navbar-icon").toggleClass("bx-x");
+  });
+
+  $(".navbar-item").click(function () {
+    $(".header .navbar-menu").removeClass("active");
+    $(".header .navbar-icon").removeClass("bx-x");
   });
 
   // Scroll Reveal
@@ -21,13 +32,7 @@ $(document).ready(function () {
 
   // Typed JS
   var typed = new Typed(".typing1", {
-    strings: [
-      "Software Developer",
-      "Web Developer",
-      "Youtuber",
-      "Pianist",
-      "Badminton Player",
-    ],
+    strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
     startDelay: 1500,
     typeSpeed: 100,
     backSpeed: 75,
@@ -36,16 +41,53 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typing2", {
-    strings: [
-      "Software Developer",
-      "Web Developer",
-      "Youtuber",
-      "Pianist",
-      "Badminton Player",
-    ],
+    strings: ["Software Developer", "Web Developer", "Youtuber", "Pianist", "Badminton Player"],
     typeSpeed: 100,
     backSpeed: 75,
     backDelay: 1500,
     loop: true,
   });
+
+  // Shop Item Display
+  let shopItems = document.querySelector(".shop .shop-items");
+  shopItems.innerHTML += shopItems.innerHTML;
+  
+  let shopBtn = document.querySelectorAll(".shop .shop-btn-container .shop-btn");
+  let speed = -2;
+
+  let move = () => {
+    // Calculate the new position
+    let currentLeft = parseInt(shopItems.style.left) || 0;
+    let newLeft = currentLeft + speed;
+
+    // Reset position if we have scrolled past half the width of the container
+    if (newLeft <= -shopItems.offsetWidth / 2) {
+      newLeft = 0; 
+    }
+    // Reset position if we have scrolled too far to the right (unlikely with negative speed)
+    if (newLeft > 0) {
+      newLeft -= shopItems.offsetWidth / 2;
+    }
+
+    // Update the position
+    shopItems.style.left = newLeft + "px";
+  };
+
+  let timer = setInterval(move, 30);
+
+  shopItems.addEventListener("mouseover", function () {
+    clearInterval(timer);
+  });
+
+  shopItems.addEventListener("mouseout", function () {
+    timer = setInterval(move, 30);
+  });
+
+  shopBtn[0].onclick = function () {
+    speed = -2;
+  };
+
+  shopBtn[1].onclick = function () {
+    speed = 2;
+  };
 });
